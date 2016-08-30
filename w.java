4d47233a405ffdab55692786c56c46c153c16cfb
vishal <vishalsenthilkumaran05@gmail.com>
2016-08-30 14:12:34 +0530

    Create w.java

---
+++ w.java
@@ -0,0 +1,22 @@
+package vish;
+
+import java.util.Scanner;
+
+public class vi {
+
+	public static void main(String[] args) 
+	{
+		Scanner in=new Scanner(System.in);
+		
+		char q;
+		q=in.nextLine().charAt(0);
+		
+		if(q==q)
+		{
+			System.exit(0);
+		}
+		
+
+	}
+
+}
