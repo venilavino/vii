05f6074ad935c8a9b3194daa1841a1de66c67fd2
vishal <vishalsenthilkumaran05@gmail.com>
2016-08-30 15:14:56 +0530

    d.java

---
+++ d.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public int maxPoints(Point[] points) {
+    int max=0;
+    if(points.length==1)
+        return 1;
+     for(int i=0;i<points.length;i++){
+         for(int j=0;j<points.length;j++){
+         if((points[i].x!=points[j].x)||(points[i].y!=points[j].y)){
+         int coll=get_collinear(points[i].x,points[i].y,points[j].x,points[j].y,points);
+                  if(coll>max)
+                    max=coll;
+                }
+                else{
+
+                    **Case where I am suffering**
+
+                }
+           }
+        }
+  return max;
+}
+public int get_collinear(int x1,int y1,int x2, int y2,Point[] points)
+{
+    int c=0;
+    for(int i=0;i<points.length;i++){
+        int k1=x1-points[i].x;
+        int l1=y1-points[i].y;
+        int k2=x2-points[i].x;
+        int l2=y2-points[i].y;
+        if((k1*l2-k2*l1)==0)
+            c++;
+    }
+    return c;
+}
+}
