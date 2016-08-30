f57e294058e6f1e8df3000adcabae0e6ebe8826c
vishal <vishalsenthilkumaran05@gmail.com>
2016-08-30 15:26:33 +0530

    cu.java

---
+++ cu.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class CK_Numeric_Validate
+{
+    public static void main(String[] args)
+    {
+       System.out.println("ENTER THE STRING:");
+       Scanner sc=new Scanner(System.in);
+       String a=new String();
+       a=sc.next();
+       int c=0;
+       char b[]=a.toCharArray();
+       for(int i=0;i<b.length;i++)
+       {
+        if(b[i]=='0'||b[i]=='1'||b[i]=='2'||b[i]=='3'||b[i]=='4'||b[i]=='5'||b[i]=='6'||b[i]=='7'||b[i]=='8'||b[i]=='9'
+         ||b[i]=='.'||b[i]=='e'||b[i]=='E'||b[i]==32)
+        {
+            c++; 
+        }
+    else
+         c--;
+   }
+    
+       if(c==b.length)
+        System.out.println("true");
+       else
+        System.out.println("false");
+    }
+}
